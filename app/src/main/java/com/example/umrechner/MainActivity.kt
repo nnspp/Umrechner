303246c31b23205340c70cac937152edf8393c67
NP <ninasopopp@gmail.com>
2025-05-31 19:29:42 +0200

    soweit mittelmäßig funktionsfähig, aber optionen 2 und 3 sind nicht richtig implementiert. ui zu verbessern, wegen diversen versuchen hinweise auf nichtgenutzte import directive nur zK hehe

--- app/src/main/java/com/example/umrechner/MainActivity.kt
+++ app/src/main/java/com/example/umrechner/MainActivity.kt
@@ -1,17 +1,43 @@
 package com.example.umrechner
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.umrechner.ui.theme.UmrechnerTheme
+
+/* Lesen lohnt sich noch nicht: Alles noch Kladderadatsch
+* Optionen 2 und 3 sind btw soweit nicht richtig implementiert */
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +46,85 @@ class MainActivity : ComponentActivity() {
         setContent {
             UmrechnerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    UnitConverter()
                 }
             }
         }
     }
+
+@Preview
+@Composable
+fun UnitConverterPreview(){
+    UnitConverter()
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnitConverter() {
+    var inputValue by remember { mutableStateOf("") }
+    var selectedConversion by remember { mutableStateOf("Konvertiere Optionen") }
+    var result by remember { mutableStateOf("") }
+    Column(modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text("Umrechner-App")
+        OutlinedTextField(label= {Text ("Wert eintragen: qm, Alter, Cent")}, value = inputValue, onValueChange ={inputValue = it})
+        Row {
+            DropdownMenufun(selectedConversion= selectedConversion, onOptionSelected={selectedConversion=it})
+            SolveButton(onClick={result=convert(inputValue,selectedConversion)})
+            }
+        Text("Ergebnis: $result")
+        }
+
+    }
+
+@Composable
+fun DropdownMenufun(selectedConversion: String, onOptionSelected: (String) -> Unit){
+    var expanded by remember { mutableStateOf(false) }
+
+        Box{
+            Spacer(modifier = Modifier.width(16.dp))
+            Button(onClick = {expanded = !expanded}) {
+                Text("Auswahl")
+                Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+            }
+            /*ständig fehler, wenn Nutzung DropdownMenuItem nach ILIAS pdf höa*/
+            DropdownMenu(
+                expanded = expanded, onDismissRequest = {expanded = false})
+                {
+                val options = listOf("qm in Fußballfeldern","Alter in Minuten","Cent in Sekunden" )
+
+                    options.forEach{
+                        option ->
+                        DropdownMenuItem(
+                            text={Text(option)},
+                            onClick = {onOptionSelected(option)
+                                expanded = false}
+                        )
+                    }
+
+                }
+
+        }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UmrechnerTheme {
-        Greeting("Android")
+fun SolveButton(onClick:() -> Unit){
+    Button(onClick=onClick){
+        Text("Ausrechnen!")
+    }
+}
+/*Option 2: ist erstmal ohne Berechnung vom genauen Alter! Option 3: idfk wtf? 1ct = 1 sek*/
+fun convert(inputValue: String, selectedConversion: String): String {
+    val value = inputValue.toDouble()
+    return try {
+        when (selectedConversion) {
+            "qm in Fußballfeldern" -> "${value / 7140} Fußballfelder"
+            "Alter in Minuten" -> "${value * 525600} Minuten"
+            "Cent in Sekunden" -> "${value * 6000} Cent"
+            else -> "Bitte wähle eine Dropdown Option aus."
+        }
+    } catch (e: NumberFormatException) {
+        "Nicht gültig"
+        }
     }
 }
