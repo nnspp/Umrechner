e55b27a31167c142a7e7962a91c0b82a61e7fa66
Gabriel Pospischil <gabriel-pospischil@t-online.de>
2025-06-04 15:09:12 +0200

    neuer branch, m² umrechner anders aufgebaut

--- app/src/main/java/com/example/umrechner/MainActivity.kt
+++ app/src/main/java/com/example/umrechner/MainActivity.kt
@@ -5,48 +5,34 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconToggleButton
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
-import androidx.compose.ui.util.fastRoundToInt
-import com.example.umrechner.ui.theme.UmrechnerTheme
-import java.math.BigDecimal
+import androidx.core.text.isDigitsOnly
 import java.math.RoundingMode
-import kotlin.math.round
-import kotlin.math.roundToLong
+import java.text.DecimalFormat
 
 /* Lesen lohnt sich noch nicht: Alles noch Kladderadatsch
 * Optionen 2 und 3 sind btw soweit nicht richtig implementiert */
@@ -56,114 +42,175 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            UmrechnerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    UnitConverter()
-                }
-            }
+            UmrechnerStart()
+
+//            UmrechnerTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    UnitConverter()
+//                }
+//            }
         }
     }
 }
 
 @Preview
 @Composable
-fun UnitConverterPreview(){
-    UnitConverter()
+fun PreviewMessageCard() {
+    UmrechnerStart()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UnitConverter() {
-    var inputValue by remember { mutableStateOf("") }
-    var selectedConversion by remember { mutableStateOf("Konvertiere Optionen") }
-    var result by remember { mutableStateOf("") }
-    Column(modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Umrechner-App")
-        Text("\n" +
-                "    Flächen in Fußballfelder: Weißte.\n" +
-                "    Alter in Minuten: Du bist X Minuten alt! \n" +
-                "    Geld in Zeit: Geldbetrag in Tage bzw. Jahre.\n")
-        OutlinedTextField(label= {Text ("Wert eintragen: qm, Alter, Cent")}, value = inputValue, onValueChange ={inputValue = it})
-        Row {
-            DropdownMenufun(selectedConversion= selectedConversion, onOptionSelected={selectedConversion=it})
-            SolveButton(onClick={result=convert(inputValue,selectedConversion)})
-            }
-        Text("Ergebnis: $result")
-        }
-
-    }
-
-@Composable
-fun DropdownMenufun(selectedConversion: String, onOptionSelected: (String) -> Unit){
-    var expanded by remember { mutableStateOf(false) }
-        Box{
-            Spacer(modifier = Modifier.width(16.dp))
-            Button(onClick = {expanded = !expanded}) {
-                Text("Auswahl")
-                Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
-            }
-            DropdownMenu(
-                expanded = expanded, onDismissRequest = {expanded = false})
-                {
-                val options = listOf("qm in Fußballfeldern","Alter in Minuten","Cent in Zeit" )
-
-                    options.forEach{
-                        option ->
-                        DropdownMenuItem(
-                            text={Text(option)},
-                            onClick = {onOptionSelected(option)
-                                expanded = false}
-                        )
-                    }
-
+fun UmrechnerStart() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Umrechner")
                 }
-
+            )
         }
+    ) {
+        innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            TestSqmConverter()
+//            TestDateConverter()
+//            TestMoneyConverter()
+        }
+    }
 }
-
 
 @Composable
-fun SolveButton(onClick:() -> Unit){
-    Button(onClick=onClick){
-        Text("Ausrechnen!")
+fun TestSqmConverter() {
+
+    Row(modifier = Modifier
+        .padding(all = 8.dp)
+        .fillMaxWidth()
+        ) {
+        var text by remember { mutableStateOf("") }
+        TextField(
+            modifier = Modifier.weight(1.95f),
+            value = text,
+            onValueChange = { text = it},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number), //damit auch nur Zahlen reinkommen
+            label = { Text(text = "m² in Fußballfelder") },
+            placeholder ={ Text(text = "Eingeben") },
+        )
+        Spacer(modifier = Modifier.weight(0.1f))
+        TestSqmButton(
+            onClick = {},
+            modifier = Modifier.weight(0.95f),
+            wert = text
+        )
     }
 }
 
-/*Option 2: ist erstmal ohne Berechnung vom genauen Alter! Option 3: idfk wtf? 1ct = 1 sek*/
-fun convert(inputValue: String, selectedConversion: String): String {
-    val value = inputValue.toDouble()
-//still unfisnished due to AARGH
-    return try {
-        when (selectedConversion) {
-            "qm in Fußballfeldern" -> "${value / 7140} Fußballfelder"
-            "Alter in Minuten" -> "${value.roundToLong() * 525600} Minuten"
-            "Cent in Zeit" -> "${value.roundToLong() /144000} Tage oder ${value.roundToLong()/51264000} Jahre"
-            else -> "Bitte wähle eine Dropdown Option aus."
-        }
-    } catch (e: NumberFormatException) {
-        "Nicht gültig"
-        }
+@Composable
+fun TestSqmButton(onClick: () -> Unit, modifier: Modifier, wert: String) {
+    val context = LocalContext.current
+    OutlinedButton(onClick = {onClick()
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.DOWN
+        val ergebnis = df.format(wert.toDouble() / 7140)
+        Toast.makeText(
+            context,
+            "Etwa $ergebnis Fußballfelder",
+            Toast.LENGTH_SHORT).show()
+    },
+    ) {
+        Text("Ausrechnen")
     }
+}
 
+@Composable
+fun TestMoneyConverter() {
+    TODO("Not yet implemented")
+}
 
-/*
-1 Jahr
-12 Monate
-365 Tage
-8760 Stunden
-525600 Minuten
-31536000 Sekunden
----
-1 Tag
-24 Stunden
-1440 Minuten
-86400 Sekunden
----
-3e-8 Jahre
-3,8e-7 Monate
-0,00001157 Tage
-0,00027778 Stunden
-0,01666667 Minuten
-1 Sekunde
- */
+@Composable
+fun TestDateConverter() {
+    TODO("Not yet implemented")
+}
+
+//@Preview
+//@Composable
+//fun UnitConverterPreview(){
+//    UnitConverter()
+//}
+//
+//@Composable
+//fun UnitConverter() {
+//    var inputValue by remember { mutableStateOf("") }
+//    var selectedConversion by remember { mutableStateOf("Konvertiere Optionen") }
+//    var result by remember { mutableStateOf("") }
+//    Column(modifier = Modifier.fillMaxSize(),
+//        verticalArrangement = Arrangement.Center,
+//        horizontalAlignment = Alignment.CenterHorizontally) {
+//        Text("Umrechner-App")
+//        OutlinedTextField(label= {Text ("Wert eintragen: qm, Alter, Cent")}, value = inputValue, onValueChange ={inputValue = it})
+//        Row {
+//            DropdownMenufun(selectedConversion= selectedConversion, onOptionSelected={selectedConversion=it})
+//            SolveButton(onClick={result=convert(inputValue,selectedConversion)})
+//            }
+//        Text("Ergebnis: $result")
+//        }
+//    }
+//
+//@Composable
+//fun DropdownMenufun(selectedConversion: String, onOptionSelected: (String) -> Unit){
+//    var expanded by remember { mutableStateOf(false) }
+//        Box{
+//            Spacer(modifier = Modifier.width(16.dp))
+//            Button(onClick = {expanded = !expanded}) {
+//                Text("Auswahl")
+//                Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+//            }
+//            /*ständig fehler, wenn Nutzung DropdownMenuItem nach ILIAS pdf höa*/ // <- was bedeutet das
+//            DropdownMenu(
+//                expanded = expanded, onDismissRequest = {expanded = false})
+//                {
+//                val options = listOf("qm in Fußballfeldern","Alter in Minuten","Cent in Sekunden" )
+//
+//                    options.forEach{
+//                        option ->
+//                        DropdownMenuItem(
+//                            text={Text(option)},
+//                            onClick = {onOptionSelected(option)
+//                                expanded = false}
+//                        )
+//                    }
+//
+//                }
+//
+//        }
+//}
+//
+//
+//@Composable
+//fun SolveButton(onClick:() -> Unit){
+//    Button(onClick=onClick){
+//        Text("Ausrechnen!")
+//    }
+//}
+//
+///*Option 2: ist erstmal ohne Berechnung vom genauen Alter! Option 3: idfk wtf? 1ct = 1 sek*/
+//fun convert(inputValue: String, selectedConversion: String): String {
+//    val value = inputValue.toDouble()
+//    return try {
+//        when (selectedConversion) {
+//            "qm in Fußballfeldern" -> "${value / 7140} Fußballfelder"
+//            "Alter in Minuten" -> "${value * 525600} Minuten"
+//            "Cent in Sekunden" -> "${value * 6000} Cent"
+//            else -> "Bitte wähle eine Dropdown Option aus."
+//        }
+//    } catch (e: NumberFormatException) {
+//        "Nicht gültig"
+//        }
+//    }
