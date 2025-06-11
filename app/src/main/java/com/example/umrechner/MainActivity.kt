661397fe8afb4ebe801f066242ba5df7d92a9c8c
Gabriel Pospischil <gabriel-pospischil@t-online.de>
2025-06-12 00:04:52 +0200

    m² in Fußballfelder fertig, struktur für datum & geld

--- app/src/main/java/com/example/umrechner/MainActivity.kt
+++ app/src/main/java/com/example/umrechner/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.umrechner
 
-import android.R.attr.value
+import android.R.attr.onClick
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -22,6 +23,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,12 +33,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.core.text.isDigitsOnly
 import java.math.RoundingMode
 import java.text.DecimalFormat
-
-/* Lesen lohnt sich noch nicht: Alles noch Kladderadatsch
-* Optionen 2 und 3 sind btw soweit nicht richtig implementiert */
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,12 +76,13 @@ fun UmrechnerStart() {
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
             SqmConverter() // Quadratmeter in Fußballfelder
-//            TestDateConverter() // Geburtsdatum -> Alter in Minuten
-//            TestMoneyConverter() // Cent in Jahre
+//            DateConverter() // Geburtsdatum -> Alter in Minuten
+            MoneyConverter() // Cent in Jahre
         }
     }
 }
 
+// hätte es gerne in mehrere funktionen ausgelagert aber bin zu blöd dafür leider
 @Composable
 fun SqmConverter() {
     Column(
@@ -89,152 +90,117 @@ fun SqmConverter() {
             .padding(all = 8.dp)
             .fillMaxWidth()
     ) {
-        var sqmErgebnis by remember { mutableStateOf("") }
+        var tempFlaeche by remember { mutableStateOf("") } // input vom Eingabefeld
+        var shownFlaeche by remember { mutableStateOf("0") } // der angezeigte input
+        var sqmErgebnis by remember { mutableStateOf("0") } // das Ergebnis
 
         // Eingabe und Button für m² in Fußballfelder
         Row(modifier = Modifier
             .padding(all = 8.dp)
             .fillMaxWidth()
         ) {
-            var text by remember { mutableStateOf("") }
             TextField(
                 modifier = Modifier.weight(1.95f),
-                value = text,
-                onValueChange = { text = it},
+                value = tempFlaeche,
+                onValueChange = { tempFlaeche = it},
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number), // beschränkt die Eingabe auf Zahlen
                 label = { Text(text = "m² in Fußballfelder") },
                 placeholder ={ Text(text = "Eingeben") },
             )
             Spacer(modifier = Modifier.weight(0.1f))
-            TestSqmButton(
-                onClick = {},
-                modifier = Modifier.weight(0.95f),
-                wert = text
-            )
+            OutlinedButton(onClick = {
+                val df = DecimalFormat("#.##")
+                df.roundingMode = RoundingMode.DOWN
+                sqmErgebnis = df.format(tempFlaeche.toDouble() / 7140)
+                shownFlaeche = tempFlaeche
+            },
+            ) {
+                Text("Ausrechnen")
+            }
         }
-        // Ergebnis
-        Text("Das sind $sqmErgebnis Fußballfelder")
+        // Ergebnis. shownText wird zusammen mit sqmErgebnis geupdated
+        Text("$shownFlaeche m² sind etwa $sqmErgebnis Fußballfelder")
     }
 
 
 }
 
+// Die App fragt das Geburtsdatum ab, berechnet das aktuelle Alter und wie vielen Minuten das entspricht.
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TestSqmButton(onClick: () -> Unit, modifier: Modifier, wert: String) {
-    val context = LocalContext.current
-    OutlinedButton(onClick = {onClick()
-        val df = DecimalFormat("#.##")
-        df.roundingMode = RoundingMode.DOWN
-        val ergebnis = df.format(wert.toDouble() / 7140)
-        Toast.makeText(
-            context,
-            "Etwa $ergebnis Fußballfelder",
-            Toast.LENGTH_SHORT).show()
-    },
+fun DateConverter() {
+    Column(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth()
     ) {
-        Text("Ausrechnen")
+        var geburtsDatum by remember { mutableStateOf(LocalDate.now()) }
+//        var formattedGeburtsDatum by remember {
+//            derivedStateOf {
+//                DateTimeFormatter
+//                    .ofPattern("MMM dd yyyy")
+//                    .format(geburtsDatum)
+//            }
+//        }
+        var tesTest by remember {mutableStateOf("")}
+
+        Row(modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth()
+        ) {
+            DatePicker(
+                state = TODO(),
+                modifier = TODO(),
+                dateFormatter = TODO(),
+                title = TODO(),
+                headline = TODO(),
+                showModeToggle = TODO(),
+                colors = TODO()
+            )
+            Spacer(modifier = Modifier.weight(0.1f))
+            OutlinedButton(onClick = {
+
+            }) {
+                Text("Ausrechnen")
+            }
+        }
+        Text("Seit GEBURTSDATUM sind 000000 Minuten vergangen.")
     }
 }
 
+// Die App fragt nach einem Geldbetrag und gibt Tage bzw. Jahre aus.
 @Composable
-fun SqmButton(onClick: () -> Unit, modifier: Modifier, wert: String) {
-    OutlinedButton(onClick = {
-        onClick()
-        calculateSqm(wert)
-    }
+fun MoneyConverter() {
+    Column(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth()
     ) {
-        Text("Ausrechnen")
+        var tempGeld by remember { mutableStateOf("") }
+        var shownGeld by remember {mutableStateOf("0")}
+        var moneyErgebnis by remember { mutableStateOf("0") }
+
+        Row(modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth()
+        ) {
+            TextField(
+                modifier = Modifier.weight(1.95f),
+                value = tempGeld,
+                onValueChange = { tempGeld = it},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number), // beschränkt die Eingabe auf Zahlen
+                label = { Text(text = "Geld") },
+                placeholder ={ Text(text = "Eingeben") },
+            )
+            Spacer(modifier = Modifier.weight(0.1f))
+            OutlinedButton(onClick = {
+                shownGeld = tempGeld
+
+            }) {
+                Text("Ausrechnen")
+            }
+        }
+        Text("$shownGeld € entspricht $moneyErgebnis Jahren")
     }
 }
 
-fun calculateSqm(wert: String): String {
-    val df = DecimalFormat ("#.##")
-    df.roundingMode = RoundingMode.DOWN
-    return df.format(wert.toDouble() / 7140)
-}
-
-@Composable
-fun TestMoneyConverter() {
-    TODO("Not yet implemented")
-}
-
-@Composable
-fun TestDateConverter() {
-    TODO("Not yet implemented")
-}
-
-//@Preview
-//@Composable
-//fun UnitConverterPreview(){
-//    UnitConverter()
-//}
-//
-//@Composable
-//fun UnitConverter() {
-//    var inputValue by remember { mutableStateOf("") }
-//    var selectedConversion by remember { mutableStateOf("Konvertiere Optionen") }
-//    var result by remember { mutableStateOf("") }
-//    Column(modifier = Modifier.fillMaxSize(),
-//        verticalArrangement = Arrangement.Center,
-//        horizontalAlignment = Alignment.CenterHorizontally) {
-//        Text("Umrechner-App")
-//        OutlinedTextField(label= {Text ("Wert eintragen: qm, Alter, Cent")}, value = inputValue, onValueChange ={inputValue = it})
-//        Row {
-//            DropdownMenufun(selectedConversion= selectedConversion, onOptionSelected={selectedConversion=it})
-//            SolveButton(onClick={result=convert(inputValue,selectedConversion)})
-//            }
-//        Text("Ergebnis: $result")
-//        }
-//    }
-//
-//@Composable
-//fun DropdownMenufun(selectedConversion: String, onOptionSelected: (String) -> Unit){
-//    var expanded by remember { mutableStateOf(false) }
-//        Box{
-//            Spacer(modifier = Modifier.width(16.dp))
-//            Button(onClick = {expanded = !expanded}) {
-//                Text("Auswahl")
-//                Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
-//            }
-//            /*ständig fehler, wenn Nutzung DropdownMenuItem nach ILIAS pdf höa*/ // <- was bedeutet das
-//            DropdownMenu(
-//                expanded = expanded, onDismissRequest = {expanded = false})
-//                {
-//                val options = listOf("qm in Fußballfeldern","Alter in Minuten","Cent in Sekunden" )
-//
-//                    options.forEach{
-//                        option ->
-//                        DropdownMenuItem(
-//                            text={Text(option)},
-//                            onClick = {onOptionSelected(option)
-//                                expanded = false}
-//                        )
-//                    }
-//
-//                }
-//
-//        }
-//}
-//
-//
-//@Composable
-//fun SolveButton(onClick:() -> Unit){
-//    Button(onClick=onClick){
-//        Text("Ausrechnen!")
-//    }
-//}
-//
-///*Option 2: ist erstmal ohne Berechnung vom genauen Alter! Option 3: idfk wtf? 1ct = 1 sek*/
-//fun convert(inputValue: String, selectedConversion: String): String {
-//    val value = inputValue.toDouble()
-//    return try {
-//        when (selectedConversion) {
-//            "qm in Fußballfeldern" -> "${value / 7140} Fußballfelder"
-//            "Alter in Minuten" -> "${value * 525600} Minuten"
-//            "Cent in Sekunden" -> "${value * 6000} Cent"
-//            else -> "Bitte wähle eine Dropdown Option aus."
-//        }
-//    } catch (e: NumberFormatException) {
-//        "Nicht gültig"
-//        }
-//    }
